2143b7f83514a2dc75c653f9735ba53c986c81e0
François Proulx <francois@boostsecurity.io>
2023-03-10 12:25:35 -0500

    boom

--- ecommerce-platform/main.firewall.tf
+++ ecommerce-platform/main.firewall.tf
@@ -2,14 +2,14 @@ resource "google_compute_network" "vpc_network" {
   name = "vpc-network"
 }
 
-# resource "google_compute_firewall" "default" {
-#   name    = "allow-rdp-access-from-the-internet"
-#   network = google_compute_network.vpc_network.name
+resource "google_compute_firewall" "default" {
+  name    = "allow-rdp-access-from-the-internet"
+  network = google_compute_network.vpc_network.name
 
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["3389"]
-#   }
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
 
-#   source_ranges = ["0.0.0.0/0"]
-# }
+  source_ranges = ["0.0.0.0/0"]
+}
