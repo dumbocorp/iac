fc08a2c324a2f95d4df52e4fcc4114ab53ee8816
François Proulx <francois@boostsecurity.io>
2023-03-10 02:33:14 -0500

    OK

--- ecommerce-platform/main.buckets.tf
+++ ecommerce-platform/main.buckets.tf
@@ -4,7 +4,7 @@ module "ecommerce_bucket" {
   source     = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version    = "~> 3.4"
   project_id = var.project
-  name       = "sudocorp-ecommerce-${random_pet.data_bucket_name.id}"
+  name       = "sudocorp-ecommerce-${random_pet.ecommerce_bucket_name.id}"
   location   = var.region
   iam_members = [
     {
--- microblogging-service/main.buckets.tf
+++ microblogging-service/main.buckets.tf
@@ -13,3 +13,4 @@ module "blogs_bucket" {
     }
   ]
 }
+
