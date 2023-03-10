f9fb9f2502237b457741aef988bf8a84b4891ed9
François Proulx <francois@boostsecurity.io>
2023-03-10 01:59:16 -0500

    OK

--- ecommerce-platform/main.buckets.tf
+++ ecommerce-platform/main.buckets.tf
@@ -1,11 +1,11 @@
 resource "random_pet" "data_bucket_name" {}
 
 module "data_bucket" {
-  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
-  version = "~> 3.4"
+  source     = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+  version    = "~> 3.4"
   project_id = var.project
-  name     = "sudocorp-${random_pet.data_bucket_name.id}"
-  location = var.region
+  name       = "sudocorp-${random_pet.data_bucket_name.id}"
+  location   = var.region
   iam_members = [
     {
       role   = "roles/storage.objectViewer"
