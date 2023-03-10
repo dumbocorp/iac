c3f0536989e9e44dee083b64d328b3dac2f41bd4
François Proulx <francois@boostsecurity.io>
2023-03-10 02:34:59 -0500

    OK

--- microblogging-service/main.buckets.tf
+++ microblogging-service/main.buckets.tf
@@ -4,7 +4,7 @@ module "blogs_bucket" {
   source     = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
   version    = "~> 3.4"
   project_id = var.project
-  name       = "sudocorp-blogs-${random_pet.data_bucket_name.id}"
+  name       = "sudocorp-blogs-${random_pet.blogs_bucket_name.id}"
   location   = var.region
   iam_members = [
     {
