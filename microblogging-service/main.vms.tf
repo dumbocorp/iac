115847acd21fb419794a2aed57b389a16cf57063
François Proulx <francois@boostsecurity.io>
2023-03-13 15:11:15 -0400

    OK

--- microblogging-service/main.vms.tf
+++ microblogging-service/main.vms.tf
@@ -1,22 +1,22 @@
 resource "random_pet" "blogs_box_name" {}
 
-resource "google_compute_instance" "blogs_box" {
-  name         = "blogs-${random_pet.blogs_box_name.id}"
-  machine_type = "e2-micro"
-  zone         = var.zone
+# resource "google_compute_instance" "blogs_box" {
+#   name         = "blogs-${random_pet.blogs_box_name.id}"
+#   machine_type = "e2-micro"
+#   zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
 
-  network_interface {
-    network = "default"
-    access_config {
-      # Setup Ephemeral public IP
-    }
-  }
+#   network_interface {
+#     network = "default"
+#     access_config {
+#       # Setup Ephemeral public IP
+#     }
+#   }
 
-  metadata_startup_script = "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"I have booted\"}' https://hooks.slack.com/services/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"
-}
+#   metadata_startup_script = "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"I have booted\"}' https://hooks.slack.com/services/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"
+# }
